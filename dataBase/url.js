b1832bd427baabaa172e9809dfa2e53fcc9906f1
root <AndersonFornalski>
2020-09-30 10:54:25 -0300

    modificado arquivos dataBase

---
+++ dataBase/url.js
@@ -0,0 +1,12 @@
+module.exports = {
+    Url_mongoDb:"mongodb+srv://drogabel:drogabel@drogabel.kj10u.mongodb.net/<dbname>?retryWrites=true&w=majority",
+
+// meu bucket de image
+    SECRET:"enaRt45ifdi8045rtUiopL",
+    AWS_SECRET_ACCESS_KEY: "iOj9FbXmlqdL7BJ39ysqXEQlxqHqXY9h0XPFE7nZ",
+    AWS_ACCESS_KEY_ID:"AKIA4DICTP5WTXTYJR66",
+    
+    S3_REGION:"us-east-2",
+    S3_BUCKET_NAME:"fornalski-software-photos"
+
+};
